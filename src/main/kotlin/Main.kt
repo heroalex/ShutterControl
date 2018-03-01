fdaa6075fa2726e053d883b658eb2554e66d5bff
heroalex <heroalex@gmx.de>
2018-03-01 16:11:00 +0100

    project setup

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+/**
+ * @author  alexander_larsson, last modified by $Author$
+ * @version SVN $Revision$ $Date$
+ * @since
+ */
+class Main {
+}
+
+fun main(args: Array<String>) {
+    println("Hello world!")
+}
